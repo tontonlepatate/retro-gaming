1f1fb609c4251cfdc035ecabf5772f5d57642cb5
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2019-06-13 11:46:55 +0200

    Ajout d'un outil de sélection

--- game.py
+++ game.py
@@ -8,20 +8,20 @@ pygame.init()
 pygame.display.set_caption("Wargame")
 
 palette = {
-    'B': [154, 14, 159], #base
-    'E': [100, 104, 236], #eau
-    'R': [0, 0, 0], #route
-    'V': [255, 0, 0], #ville
-    'f': [10, 77, 15], #foret
-    'S': [208, 182, 77],#plage
-    'M': [88, 64, 9],#montagne
-    'C': [175, 149, 88],#colline
-    'P': [66, 164, 36],#plaine
-    'F': [87, 30, 5],#fort
-    'T':[255, 255, 255],#recherche
-    'U':[255, 255, 255],#Usine M
-    'u':[255, 255, 255],#Usine C
-    ' ': [255, 255, 255]#void
+    'B': [154, 14, 159],  # base
+    'E': [100, 104, 236],  # eau
+    'R': [0, 0, 0],  # route
+    'V': [255, 0, 0],  # ville
+    'f': [10, 77, 15],  # foret
+    'S': [208, 182, 77],  # plage
+    'M': [88, 64, 9],  # montagne
+    'C': [175, 149, 88],  # colline
+    'P': [66, 164, 36],  # plaine
+    'F': [87, 30, 5],  # fort
+    'T': [255, 255, 255],  # recherche
+    'U': [255, 255, 255],  # Usine M
+    'u': [255, 255, 255],  # Usine C
+    ' ': [255, 255, 255]  # void
 }  # initialise un dictionnaire
 
 black = (0, 0, 0, 255)
@@ -99,13 +99,21 @@ terrain_units = [
         "deplacement": 10
     },
     {
+        "type": "tank",
+        "X": 0,
+        "Y": 0,
+        "deplacement": 10
+    },
+    {
         "type": "fusilier",
         "X": 25,
-        "Y": 15,
+        "Y": 10,
         "deplacement": 10
     },
 ]
 done = False
+
+selected_unit = -1
 
 while not done:
     for event in pygame.event.get():
@@ -126,12 +134,31 @@ while not done:
             couleur = LABY[ix, iy]
             pygame.draw.rect(screen, couleur, [xpix, ypix, case_size, case_size])
 
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        pos = pygame.mouse.get_pos()
+        x = pos[0]
+        y = pos[1]
+        for unite in terrain_units:
+            xu = unite["X"] * case_size
+            yu = unite["Y"] * case_size
+            rectcol = Rect(xu, yu, case_size, case_size)
+            if rectcol.collidepoint(x, y):
+                selected_unit = terrain_units.index(unite)
+        print()
+
     # Affichage des unités
     for unite in terrain_units:
+        id = terrain_units.index(unite)
+        if selected_unit == id:
+            tank = Rect(unite["X"] * case_size, unite["Y"] * case_size, case_size, case_size)
+            rect(screen, [255, 100, 0], tank)
+
         if unite["type"] == "fusilier":
-            circle(screen, [255, 0, 0], (int((unite["X"] - 0.5) * case_size), int((unite["Y"] - 0.5) * case_size)), 20)
-        if unite["type"] == "tank":
-            rect(screen, [255, 255, 0], Rect((unite["X"] * case_size) + 10, (unite["Y"] * case_size) + 10, 40, 40))
+            circle(screen, [255, 0, 255], (int((unite["X"] + 0.5) * case_size), int((unite["Y"] + 0.5) * case_size)),
+                   20)
+        elif unite["type"] == "tank":
+            tank = Rect((unite["X"] * case_size) + case_size // 4, (unite["Y"] * case_size) + case_size // 4, case_size // 2, case_size // 2)
+            rect(screen, [255, 255, 0], tank)
     clock.tick(30)
 
     pygame.display.flip()
